1dc4e1a255b96512c9eec02b87bad513cfa8f996
AnaMari <anamariamarindelgadoFSD@gmail.com>
2023-12-02 12:32:44 +0100

    first commit. Pasos del 1-4

---
+++ main.js
@@ -0,0 +1,96 @@
+import './style.css'
+
+// para hacer el tablero:
+// 1. Inicializar el canvas:
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+// vamos a escalar para que cada punto de nuestro canvas utilice el block size para la x y la y
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// 3. Hacer el board: (14x30) (los 0 son los huecos y los 1 donde hay piezas)
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1]
+]
+
+// 4. Pieza player:
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: [
+    [1, 1],
+    [1, 1]
+  ]
+}
+
+// 2. Game loop:
+function update () {
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+function draw () {
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = 'yellow'
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value) {
+        context.fillStyle = 'red'
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+      }
+    })
+  })
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') piece.position.x--
+  if (event.key === 'ArrowRight') piece.position.x++
+  if (event.key === 'ArrowDown') piece.position.y++
+})
+
+update()
