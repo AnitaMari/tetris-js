9bbf489ad97cbee48187b299e4aa67fbbf2c325c
AnaMari <anamariamarindelgadoFSD@gmail.com>
2023-12-02 12:49:03 +0100

    con colisión

--- main.js
+++ main.js
@@ -88,9 +88,38 @@ function draw () {
 }
 
 document.addEventListener('keydown', event => {
-  if (event.key === 'ArrowLeft') piece.position.x--
-  if (event.key === 'ArrowRight') piece.position.x++
-  if (event.key === 'ArrowDown') piece.position.y++
+  if (event.key === 'ArrowLeft') {
+    piece.position.x--
+    if (checkCollision()) {
+      piece.position.x++
+    }
+  }
+
+  if (event.key === 'ArrowRight') {
+    piece.position.x++
+    if (checkCollision()) {
+      piece.position.x--
+    }
+  }
+
+  if (event.key === 'ArrowDown') {
+    piece.position.y++
+    if (checkCollision()) {
+      piece.position.y--
+    }
+  }
 })
+
+// 4. Para las colisiones:
+function checkCollision () {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 &&
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      )
+    })
+  })
+}
 
 update()
